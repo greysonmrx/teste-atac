a1f89dfdfe161db118dfc0cce239fef1a25f083c
Greyson Filho <greysonmrx@gmail.com>
2021-03-26 14:23:20 -0300

    feat(backend): create route to create new todos

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -5,4 +5,8 @@ import '@modules/users/providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+import ITodosRepository from '@modules/todos/repositories/ITodosRepository';
+import TodosRepository from '@modules/todos/infra/typeorm/repositories/TodosRepository';
+
 container.registerSingleton<IUsersRepository>('UsersRepository', UsersRepository);
+container.registerSingleton<ITodosRepository>('TodosRepository', TodosRepository);
--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import todosRoutes from '@modules/todos/infra/http/routes/todos.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
+routes.use('/todos', todosRoutes);
 
 export default routes;
